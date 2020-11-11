1095f4a9c76ec0787fe550c20e15972e79ee2353
Michael Tandy <mike.tandy.1@gmail.com>
2020-11-11 19:44:30 +0000

    commit

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var expect      = require('chai').expect;
 var cors        = require('cors');
 
 var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
 
 var app = express();
 
